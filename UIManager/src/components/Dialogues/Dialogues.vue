fe451ee25dfbdf14489527dd3744b505075b6547
Jonatan Pietroski <jonatanpietroski@gmail.com>
2022-07-07 20:02:03 -0300

    Adds more information about the dialogue coming from DB now.

--- UIManager/src/components/Dialogues/Dialogues.vue
+++ UIManager/src/components/Dialogues/Dialogues.vue
@@ -1,8 +1,28 @@
 
 <template>
   <div>
-    <div>
-      {{dialogues[0].name}}
+    <div v-for="dialogue in dialogues" :key="dialogue.id">
+        {{dialogue.name}}
+      <div v-for="chat in dialogue.chat" :key="chat.dialogue_id">
+        <label for="">Is Left Talking</label><input type="checkbox" v-model="chat.left">
+        <br/>
+        <label for="">Left Character Name</label>
+        <input type="text" v-model="chat.leftName">
+        <br/>
+        <label for="">Left Portrait File Name</label>
+        <input type="text" v-model="chat.leftPortraitName">
+        <br/>
+
+        <label for="">Is Right Talking</label><input type="checkbox" v-model="chat.right">
+        <br/>
+        <label for="">Right Character Name</label>
+        <input type="text" v-model="chat.rightName">
+        <br/>
+        <label for="">Right Portrait File Name</label>
+        <input type="text" v-model="chat.rightPortraitName">
+        <br/>
+        <textarea rows="3" v-model="chat.message"></textarea>
+      </div>
     </div>
   </div>
 </template>
