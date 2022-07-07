21f870885be68fb8b6c3624cac35580ab7cae561
Jonatan Pietroski <jonatanpietroski@gmail.com>
2022-07-07 18:49:51 -0300

    Adds the Vue UI Manager.

---
+++ UIManager/src/components/Dialogues/Dialogues.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div>
+    <div>
+      {{dialogues[0].name}}
+    </div>
+  </div>
+</template>
+
+<script src="./Dialogues.js"></script>
+<style src="./Dialogues.css" scoped></style>
---
+++ UIManager/src/components/Home/HomeComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Dialogues from '@/components/Dialogues/Dialogues.vue'
+</script>
+
+<template>
+    <div>
+        <Dialogues/>
+    </div>
+</template>
