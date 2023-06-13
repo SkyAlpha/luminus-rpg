43fec4743d1e326c3f738fe76f6788e5a0aec3c5
Jonatan Pietroski <jonatanpietroski@gmail.com>
2023-06-13 14:55:40 -0300

    Removes the server.

--- server/middleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-    app.use(
-        require('cors')({
-            origin: `*`,
-        })
-    );
-};
--- server/routes.js
+++
@@ -1,9 +0,0 @@
-const ApiRoutes = require('express').Router();
-const { API_PREFIX } = require('./consts');
-const DialogueRoutes = require('./routes/dialogue.routes');
-
-ApiRoutes.use(DialogueRoutes);
-
-module.exports = (app) => {
-    app.use(API_PREFIX, ApiRoutes);
-};
