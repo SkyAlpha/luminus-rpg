b176e6846b4f5aa64a95086df4706a089bfb7787
Jonatan Pietroski <jonatanpietroski@gmail.com>
2023-06-13 14:53:34 -0300

    Adds back the missing chat.

--- src/consts/DB_SEED/Chats.js
+++ src/consts/DB_SEED/Chats.js
@@ -1,7 +1,21 @@
+import BadChair from './chats/BadChair';
 import BedRest from './chats/BedRest';
+import DungeonEntrance from './chats/DungeonEntrance';
+import HousePlates from './chats/HousePlates';
 import IntroductionChat from './chats/IntroductionChat';
+import LakeView from './chats/LakeView';
 import LogsInfo from './chats/LogsInfo';
 import LumberjackHouse from './chats/LumberjackHouse';
 import RoomBooksInfo from './chats/RoomBooksInfo';
 
-export const CHATS = [IntroductionChat, LumberjackHouse, LogsInfo, RoomBooksInfo, BedRest];
+export const CHATS = [
+	IntroductionChat,
+	LumberjackHouse,
+	LogsInfo,
+	RoomBooksInfo,
+	BedRest,
+	BadChair,
+	DungeonEntrance,
+	HousePlates,
+	LakeView,
+];
---
+++ src/consts/DB_SEED/chats/BadChair.js
@@ -0,0 +1,14 @@
+export default {
+	id: 6,
+	chat: [
+		{
+			leftPortraitName: 'lucius_portrait_beard',
+			leftName: 'Lucius',
+			left: true,
+			message: "- This is a weak chair. I think it's better not to sit on it.",
+			rightPortraitName: '',
+			rightName: '',
+			right: false,
+		},
+	],
+};
